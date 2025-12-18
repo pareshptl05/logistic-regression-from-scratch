86e5503e887d127740a949c655511d14dd1104c0
Paresh Patel <pareshptl05@gmail.com>
2025-12-18 09:44:56 +0530

    Initial commit: logistic regression from scratch

---
+++ src/losses.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def binary_cross_entropy(y,y_hat):
+    """
+    Binary cross entropy loss
+    """
+    eps = 1e-15
+    y_hat = np.clip(y_hat,eps,1-eps)
+    loss = -((y * (np.log(y_hat))) + ((1-y) * (np.log(1-y_hat))))
+    return np.mean(loss)
+
